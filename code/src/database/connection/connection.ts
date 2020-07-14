a3d6df11d493cc9387d57fa0db3c90ee7017c216
WesleyGolembiewski <wesleyarizio26@gmail.com>
2020-07-13 23:12:36 -0300

    git add db connection

---
+++ code/src/database/connection/connection.ts
@@ -0,0 +1,13 @@
+import knex from 'knex';
+
+const connection = knex({
+        client: 'pg',
+        connection: {
+            host: 'localhost',
+            user:   'postgres',
+            password: 'toor',
+            database: 'tauria'
+        }
+});
+
+module.exports = connection;
