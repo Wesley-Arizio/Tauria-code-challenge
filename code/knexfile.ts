c4650481d77dfc3b13c873852aa6e246d4049f77
WesleyGolembiewski <wesleyarizio26@gmail.com>
2020-07-14 00:50:26 -0300

    add seed directory

--- code/knexfile.ts
+++ code/knexfile.ts
@@ -2,16 +2,25 @@
 
 module.exports = {
   development: {
+    
     client: 'pg',
+
     connection: {
       database: 'tauria',
       user: 'postgres',
       password: 'toor',
     },
+
     searchPath: ['knex', 'private'],
+
     migrations: {
       schemaName: 'private',
       directory: `${__dirname}/src/database/migration`,
+    },
+
+    seeds: {
+      directory: `${__dirname}/src/database/seed`,
     }
+
   }
 };
