2dcbb95aadfa7f8e45a4e77b244755ab8fd9c5fd
WesleyGolembiewski <wesleyarizio26@gmail.com>
2020-07-15 16:12:50 -0300

    add user hash to be create in seed

--- code/src/database/seed/001_insert_user.ts
+++ code/src/database/seed/001_insert_user.ts
@@ -1,13 +1,23 @@
 import * as Knex from "knex";
+import encrypt from '../../dataService/user';
+
+import { User } from '../../controller/userController/userController';
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
     await knex("user").del();
 
+    const user = await encrypt.userGenerateHash({
+        email: 'wesley@gmail.com',
+        name: 'wesleyGolembiewski',
+        password: '734i84urf7e'
+    }) as User;
+
     // Inserts seed entries
     await knex("user").insert([
         { id: 1, email: "marcos@gmail.com", name: "Marcos", password: "123456789" },
         { id: 2, email: "andre@gmail.com",  name: "Andre",  password: "123456789" },
-        { id: 3, email: "joana@gmail.com",  name: "Joana",  password: "123456789" }
+        { id: 3, email: "joana@gmail.com",  name: "Joana",  password: "123456789" },
+        { id: 4, email: user.email, name: user.name, password: user.password      }
     ]);
 };
