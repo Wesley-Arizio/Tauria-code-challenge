8dc20eb0ae1ec5052d3fd4ab85ab80eb041769e0
WesleyGolembiewski <wesleyarizio26@gmail.com>
2020-07-15 20:27:18 -0300

    add function to get all users

--- code/src/controller/userController/userController.ts
+++ code/src/controller/userController/userController.ts
@@ -1,10 +1,11 @@
 import knex from '../../database/connection/connection';
 import { Request, Response } from 'express';
+import crypto from 'crypto-js'
+
 
 import userValidate from '../../schemaValidation/user';
 
-
-interface User {
+export interface User {
     email: string,
     name: string,
     password: string
@@ -12,11 +13,10 @@ interface User {
 
 class UserController {
 
-    async validateData(req: Request, res: Response){
+    async createUser(req: Request, res: Response){
 
         const { value, error } =  await userValidate(req.body);
         const user: User = value;
-
 
         if(error) {
             return res.status(412).send({
@@ -27,26 +27,64 @@ class UserController {
 
         const userCount: any =  await knex('user').where('email', user.email);
 
+        
         if(userCount.length > 0){
             return res.status(412).send({
                 message: "User already exist"
             });
         }
 
+        
         try{
-            const userRegistered = await knex
-                .insert({ email: user.email ,name: user.name, password: user.password })
-                .table('user');
-
-
-            return res.status(200).send(userRegistered);
+            const hashPassword =  crypto.SHA256(user.password).toString(crypto.enc.Hex); 
+            await knex('user')
+                .insert({
+                    email: user.email,
+                    name: user.name,
+                    password: hashPassword
+                })
+                .then(() => {  
+                    return res.status(200).send({
+                        message: 'User has been created: ',
+                    });
+                })
+                .catch(error => {
+                    return res.status(400).send({
+                        message: 'Error creating user',
+                        error 
+                    });
+                });
         }catch(error){
-            return res.status(400).send({
-                message: "Error on registering an user, please try again",
+            return {
+                message: "Error validating user",
                 error
-            });
+            }
         }
-    }   
+    }  
+    
+    async getUsers(req: Request, res: Response){
+        try {
+            await knex('user')
+                .select('email', 'name')
+                    .then(users => {
+                        return res.status(200).send({
+                            message: 'You can see all users',
+                            users
+                        });
+                    })
+                    .catch(error => {
+                        return res.status(400).send({
+                            message: 'Cannot get any user: ',
+                            error
+                        });
+                    });
+        } catch(error) {
+            return {
+                message: "Error on getting all users: ",
+                error
+            }
+        }
+    }
 }
 
 export default new UserController;
