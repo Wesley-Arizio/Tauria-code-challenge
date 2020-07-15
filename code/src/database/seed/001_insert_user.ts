44d8ea9062c02f1b7c40da5e37c1a17532b1718b
WesleyGolembiewski <wesleyarizio26@gmail.com>
2020-07-15 02:02:06 -0300

    add email value to insert

--- code/src/database/seed/001_insert_user.ts
+++ code/src/database/seed/001_insert_user.ts
@@ -6,8 +6,8 @@ export async function seed(knex: Knex): Promise<void> {
 
     // Inserts seed entries
     await knex("user").insert([
-        { id: 1, name: "Marcos", password: "1234567" },
-        { id: 2, name: "André",  password: "1234567" },
-        { id: 3, name: "Joana",  password: "1234567" }
+        { id: 1, email: "marcos@gmail.com", name: "Marcos", password: "123456789" },
+        { id: 2, email: "andre@gmail.com",  name: "Andre",  password: "123456789" },
+        { id: 3, email: "joana@gmail.com",  name: "Joana",  password: "123456789" }
     ]);
 };
