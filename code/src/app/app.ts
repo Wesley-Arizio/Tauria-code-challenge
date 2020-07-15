a3417af970a9b7ab424772d594fc1a12e3ecde08
WesleyGolembiewski <wesleyarizio26@gmail.com>
2020-07-15 20:35:29 -0300

    change port

--- code/src/app/app.ts
+++ code/src/app/app.ts
@@ -1,6 +1,6 @@
 import app from './server'
 
 
-app.listen(3001, () => {
-    console.log('Server is running on port 3001');
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
