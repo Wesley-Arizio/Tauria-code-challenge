a9e0d3cb104391cd4851573e8a96e4192fb02f3a
WesleyGolembiewski <wesleyarizio26@gmail.com>
2020-07-15 22:53:56 -0300

    change endpoint name

--- code/src/app/routes.ts
+++ code/src/app/routes.ts
@@ -17,6 +17,6 @@ routes.post('/login', (req, res) => {
 routes.get('/users', userController.getUsers);
 
 // route to get user by name
-routes.get('/username', userController.getUserByName);
+routes.get('/user', userController.getUserByName);
 
 export default routes;
