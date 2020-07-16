9e0b53df33ac5851c574008ed07ead3bce6c4612
WesleyGolembiewski <wesleyarizio26@gmail.com>
2020-07-16 17:35:08 -0300

    add route to delete an user

--- code/src/app/routes/private.ts
+++ code/src/app/routes/private.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import authenticate from '../../middleware/routeAuthenticate';
 import userController from '../../controller/userController/userController';
+import userValidate from '../../schemaValidation/user';
 
 const privateRoute = express.Router();
 
 declare global {
         namespace Express {
             export interface Request {
-                userId: number
+                userId: any
         }
     }
  }
@@ -19,6 +20,12 @@ privateRoute.get('/home', (req, res) => {
     return res.status(200).send();
 })
 
+
+// update user
 privateRoute.put('/user', userController.updateUser);
+
+//delete user
+privateRoute.delete('/user', userController.deleteUser);
+
 
 export default privateRoute;
