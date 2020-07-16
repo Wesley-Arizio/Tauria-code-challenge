f83fb30d9143b9240a6dbcb431ad8eca453a32fd
WesleyGolembiewski <wesleyarizio26@gmail.com>
2020-07-16 16:23:51 -0300

    add new property to Request, add route to update user account

--- code/src/app/routes/private.ts
+++ code/src/app/routes/private.ts
@@ -1,12 +1,24 @@
 import express from 'express';
-
-import  userController from '../../controller/userController/userController';
-
+import authenticate from '../../middleware/routeAuthenticate';
+import userController from '../../controller/userController/userController';
 
 const privateRoute = express.Router();
 
-privateRoute.get('/routet/private', (req, res) => {
-    res.send("ok");
+declare global {
+        namespace Express {
+            export interface Request {
+                userId: number
+        }
+    }
+ }
+
+
+privateRoute.use(authenticate)
+
+privateRoute.get('/home', (req, res) => {
+    return res.status(200).send();
 })
+
+privateRoute.put('/user', userController.updateUser);
 
 export default privateRoute;
