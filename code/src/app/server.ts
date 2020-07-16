e0dfd5e6089bc96ab7b943d0d9fd7361f842efc9
WesleyGolembiewski <wesleyarizio26@gmail.com>
2020-07-16 11:41:57 -0300

    change routes directory

--- code/src/app/server.ts
+++ code/src/app/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
-import routes from './routes';
+import publicRoute from './routes/public';
+import privateRoute from './routes/private'
 
 const app = express();
 
 app.use(express.json());
-app.use(routes);
+
+app.use(publicRoute);
+app.use(privateRoute);
 
 export default app;
