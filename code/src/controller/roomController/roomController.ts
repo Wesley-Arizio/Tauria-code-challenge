34acae61edd22904cc71fe05f165720b78243b15
WesleyGolembiewski <wesleyarizio26@gmail.com>
2020-07-17 06:39:00 -0300

    add function to enter in a room

--- code/src/controller/roomController/roomController.ts
+++ code/src/controller/roomController/roomController.ts
@@ -58,9 +58,6 @@ class RoomController{
                 message: 'Room conference has been created'
             });
 
-            // return res.status(200).send({
-            //     message: 'Room conference has been created'
-            // });
         } catch (error) {
             return res.status(400).send({
                 message: 'Error on creating a new room confernece',
@@ -68,6 +65,57 @@ class RoomController{
             })
         }
     }
+
+    async getAllRooms(req: Request, res: Response){
+        const list = await knex('room')
+            .select("*");
+
+        return res.status(200).send({
+            list
+        })
+    }
+
+    async enterInRoom(req: Request, res: Response){
+        const room_id = req.params.room_id; 
+        const user_isd = req.userId;
+
+        const capacity = (await knex('room').select('capacity').where('id', room_id))[0].capacity;
+
+        const allUsersIn = await knex('user_room').select('user_id').where('room_id', room_id);
+ 
+        if(capacity <= allUsersIn.length){
+            return res.status(400).send({
+                message: 'You cannot enter, this conference is full'
+            });
+        }
+        
+        const isValid = allUsersIn.filter( item => {
+            return item.user_id === user_isd
+        });
+
+
+        if(isValid.length > 0){
+            return res.status(400).send({
+                message: "You're already in this room"
+            })
+        }
+
+        try {
+            await knex('user_room').insert({
+                user_id: user_isd,
+                room_id: room_id
+            });
+    
+            return res.status(200).send({
+                message: "You're in"
+            })
+        } catch (error){
+            return res.status(400).send({
+                message: "Error on inserting you into this room"
+            });
+        }
+    }
+
 
 }
 
